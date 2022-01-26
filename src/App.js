1bb71108324c0ad636c79b6dce50d8b991a49ada
NicksyRop <nicksonkipkorir25@gmail.com>
2022-01-27 01:07:37 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function App() {
+function App(props) {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filterCountries, setFilteredContries] = useState([]);
+
+  const handleChange = (event) => {
+    const val = event.target.value.toLowerCase();
+
+    setSearch(val);
+    let result = [];
+
+    result = countries.filter((data) => {
+      return data.name.comon.search(val) != -1;
+    });
+
+    setFilteredContries(result);
+  };
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      setCountries(res.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: "center", color: "blue" }}>
+        Countries Details App
+      </h1>
+      <label>Find Countries</label> <br />
+      <input type="text" value={search} onChange={handleChange}></input> <br />
+      {filterCountries.map((country) => (
+        <li key={country.name}>{country.name} </li>
+      ))}
     </div>
   );
 }
